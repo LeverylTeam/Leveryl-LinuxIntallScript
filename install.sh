46bbf36ca6e5ed38916ed813c64e1c21d8986d59
CortexPE <mcpe4life62@gmail.com>
2017-06-16 23:33:45 -0500

    Update

--- install.sh
+++ install.sh
@@ -21,30 +21,30 @@ else
 fi
 time=$(date +"%T")
 echo "$time [INFO] Downloading Latest Github Build..."
-wget https://github.com/LeverylTeam/LeverylTeam/Leveryl/archive/master.zip
-chmod 777 master.zip
+wget https://github.com/LeverylTeam/LeverylTeam/Leveryl/archive/master.zip >/dev/null
+chmod 777 master.zip >/dev/null 2>
 time=$(date +"%T")
 echo "$time [INFO] Un-Zipping..."
-unzip -o master.zip
-chmod 777 Leveryl-master
-cd Leveryl-master
-chmod 777 src
+unzip -o master.zip >/dev/null 2>
+chmod 777 Leveryl-master >/dev/null 2>
+cd Leveryl-master >/dev/null 2>
+chmod 777 src >/dev/null 2>
 time=$(date +"%T")
 echo "$time [INFO] Copying Files..."
-cp -rf src ..
-chmod +x start.sh
-cp -rf start.sh ..
+cp -rf src .. >/dev/null 2>
+chmod +x start.sh >/dev/null 2>
+cp -rf start.sh .. >/dev/null 2>
 time=$(date +"%T")
 echo "$time [INFO] Cleaning up..."
-cd ..
-rm -rf Leveryl-master
-rm -rf master.zip
-wget --no-check-certificate https://raw.githubusercontent.com/LeverylTeam/Leveryl/master/LICENSE.md
+cd .. >/dev/null 2>
+rm -rf Leveryl-master >/dev/null 2>
+rm -rf master.zip >/dev/null 2>
+wget --no-check-certificate https://raw.githubusercontent.com/LeverylTeam/Leveryl/master/LICENSE.md >/dev/null 2>
 time=$(date +"%T")
 echo "$time [INFO] Installing x64 PHP binary..."
-wget --no-check-certificate https://bintray.com/artifact/download/pocketmine/PocketMine/PHP_7.0.2_x86-64_Linux.tar.gz
-chmod 777 PHP_7.0.2_x86-64_Linux.tar.gz
-tar zxvf PHP_7.0.2_x86-64_Linux.tar.gz
-rm -r PHP_7.0.2_x86-64_Linux.tar.gz
+wget --no-check-certificate https://bintray.com/artifact/download/pocketmine/PocketMine/PHP_7.0.2_x86-64_Linux.tar.gz >/dev/null 2>
+chmod 777 PHP_7.0.2_x86-64_Linux.tar.gz >/dev/null 2>
+tar zxvf PHP_7.0.2_x86-64_Linux.tar.gz >/dev/null 2>
+rm -r PHP_7.0.2_x86-64_Linux.tar.gz >/dev/null 2>
 time=$(date +"%T")
 echo "$time [NOTICE] Installation Finished!"
