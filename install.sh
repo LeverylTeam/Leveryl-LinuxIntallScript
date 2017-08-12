0249adecee798908f422ea1039b0e12db64d5218
CortexPE <CortexPE@users.noreply.github.com>
2017-08-12 05:40:35 -0500

    Fix Install Errors.

--- install.sh
+++ install.sh
@@ -1,50 +1,53 @@
 #!/bin/bash
-time=$(date +"%T")
+clear
+time=$(date +"%T") &> /dev/null
 echo "$time [INFO] Starting Leveryl installation..."
-time=$(date +"%T")
+time=$(date +"%T") &> /dev/null
 echo "$time [INFO] Checking for Dependancies"
 if [ -f /etc/debian_version ]; then
-	time=$(date +"%T")
+        time=$(date +"%T")
     command -v wget >/dev/null 2>&1 || { echo "$time [ERROR] WGET Command not Found, Installing WGET..." >&2; apt-get -y install wget;}
-	time=$(date +"%T")
+        time=$(date +"%T")
     command -v unzip >/dev/null 2>&1 || { echo "$time [ERROR] UNZIP Command not Found, Installing UNZIP..." >&2; apt-get -y install unzip;}
 elif [ -f /etc/redhat-release ]; then
-	time=$(date +"%T")
+        time=$(date +"%T")
     command -v wget >/dev/null 2>&1 || { echo "$time [ERROR] WGET Command not Found, Installing WGET..." >&2; yum -y install wget;}
-	time=$(date +"%T")
+        time=$(date +"%T")
     command -v unzip >/dev/null 2>&1 || { echo "$time [ERROR] UNZIP Command not Found, Installing UNZIP..." >&2; yum -y install unzip;}
 else
-	time=$(date +"%T")
+        time=$(date +"%T")
     command -v wget >/dev/null 2>&1 || { echo "$time [ERROR] WGET Command not Found, Installing WGET..." >&2; apt-get -y install wget;}
-	time=$(date +"%T")
+        time=$(date +"%T")
     command -v unzip >/dev/null 2>&1 || { echo "$time [ERROR] UNZIP Command not Found, Installing UNZIP..." >&2; apt-get -y install unzip;}
 fi
-time=$(date +"%T")
+time=$(date +"%T") &> /dev/null
 echo "$time [INFO] Downloading Latest Github Build..."
-wget --no-check-certificate https://github.com/LeverylTeam/Leveryl/archive/master.zip
-chmod 777 master.zip
-time=$(date +"%T")
+wget --no-check-certificate https://github.com/LeverylTeam/Leveryl/archive/master.zip &> /dev/null
+chmod 777 master.zip &> /dev/null
+time=$(date +"%T") &> /dev/null
 echo "$time [INFO] Un-Zipping..."
-unzip -o master.zip
-chmod 777 Leveryl-master
-cd Leveryl-master
-chmod 777 src
-time=$(date +"%T")
+unzip -o master.zip &> /dev/null
+chmod 777 Leveryl-master &> /dev/null
+cd Leveryl-master &> /dev/null
+chmod 777 src &> /dev/null
+time=$(date +"%T") &> /dev/null
 echo "$time [INFO] Copying Files..."
-cp -rf src ..
-chmod +x start.sh
-cp -rf start.sh ..
-time=$(date +"%T")
+cp -rf src .. &> /dev/null
+chmod +x start.sh &> /dev/null
+cp -rf start.sh .. &> /dev/null
+time=$(date +"%T") &> /dev/null
 echo "$time [INFO] Cleaning up..."
-cd ..
-rm -rf Leveryl-master
-rm -rf master.zip
-wget --no-check-certificate https://raw.githubusercontent.com/LeverylTeam/Leveryl/master/LICENSE
-time=$(date +"%T")
+cd .. &> /dev/null
+rm -rf Leveryl-master &> /dev/null
+rm -rf master.zip &> /dev/null
+time=$(date +"%T") &> /dev/null
 echo "$time [INFO] Installing x64 PHP binary..."
-wget --no-check-certificate https://raw.githubusercontent.com/LeverylTeam/PHP7-Binaries/master/PHP-7.0.14_Linux-x84_64.zip
-chmod 777 PHP_7.0.2_x86-64_Linux.tar.gz
-tar zxvf PHP_7.0.2_x86-64_Linux.tar.gz
-rm -r PHP_7.0.2_x86-64_Linux.tar.gz
-time=$(date +"%T")
+wget --no-check-certificate https://raw.githubusercontent.com/LeverylTeam/PHP7-Binaries/master/PHP-7.0.14_Linux-x84_64.zip &> /dev/null
+chmod 777 PHP-7.0.14_Linux-x84_64.zip &> /dev/null
+unzip -o PHP-7.0.14_Linux-x84_64.zip &> /dev/null
+rm -r PHP-7.0.14_Linux-x84_64.zip &> /dev/null
+chmod +x start.sh &> /dev/null
+time=$(date +"%T") &> /dev/null
 echo "$time [NOTICE] Installation Finished!"
+time=$(date +"%T") &> /dev/null
+echo "$time [NOTICE] Type ./start.sh to start!"
